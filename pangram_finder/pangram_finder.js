4b69b3e9b66558f89228d78269793916e1a3f708
Cody Abbott <cody.abbott16@gmail.com>
2020-01-08 20:10:05 +0000

    finished pangram question

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,15 @@
 const PangramFinder = function (phrase) {
+  this.phrase = phrase;
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
 }
 
 PangramFinder.prototype.isPangram = function () {
-
-}
+  let characterArray = this.phrase.split('');
+  const cleanedArray = characterArray.map((letter) => {
+    return letter.toLowerCase();
+  });
+  const letterCheck = this.alphabet.every(value => cleanedArray.includes(value)); 
+  return letterCheck;
+};
 
 module.exports = PangramFinder;
