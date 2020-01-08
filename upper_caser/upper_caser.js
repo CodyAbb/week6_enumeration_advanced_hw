79d482db2908b4232b5859625bc9394c4ee35270
Cody Abbott <cody.abbott16@gmail.com>
2020-01-08 16:25:21 +0000

    passes anagram tests

--- upper_caser/upper_caser.js
+++ upper_caser/upper_caser.js
@@ -1,8 +1,9 @@
-const UpperCaser = function (words) {
+const UpperCaser = function (wordArray) {
+    this.wordArray = wordArray;
 }
 
 UpperCaser.prototype.toUpperCase = function () {
-
+    return this.wordArray.map(word => word.toUpperCase())
 }
 
 module.exports = UpperCaser;
