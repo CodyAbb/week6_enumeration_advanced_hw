39bcaea0e6472b10c8e2d7f9d6e678f2cc5546ef
Cody Abbott <cody.abbott16@gmail.com>
2020-01-08 21:59:00 +0000

    finished mvp

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,20 @@
 const IsogramFinder = function (word) {
-
+    this.word = word.split('')
 }
 
 IsogramFinder.prototype.isIsogram = function () {
+    const cleanedArray = this.word.map((letter) => {
+        return letter.toLowerCase();
+    });
 
+    const checkRepeats = arr => arr.filter((item, index) => arr.indexOf(item) != index);
+
+    const result = checkRepeats(cleanedArray);
+    if (result.length > 0) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 module.exports = IsogramFinder;
